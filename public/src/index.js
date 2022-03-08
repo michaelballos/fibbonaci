38bbf6857e78fefaad724894662b4de3eed50a49
michaelballos <mdb3091207@gmail.com>
2022-03-08 15:31:35 -0600

    Js fibonacci function revise
    Fixed repeated statements and simplified code

--- public/src/index.js
+++ public/src/index.js
@@ -1,16 +1,8 @@
-const fibSeq = function fib(num) { 
-  if (num == 1) {
-    return 1; 
-   
-  }
-  if (num == 1) {
-    return 1;
-  }
-  else {
-    return fib(num - 1) + fib(num - 2);
-  }
-};
-
-
 //[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...]
+function fib(num) { 
+  if (num <= 1) 
+    return 1;
+    return fib(num - 1) + fib(num - 2);
+}
 
+console.log(fib(5));
