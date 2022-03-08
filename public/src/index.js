355a6dec4a9e13429205ce86fdb670b298853247
michaelballos <mdb3091207@gmail.com>
2022-03-08 17:57:18 -0600

    Define array length
    Change array function to define the array length to add '...' to the end of the array

--- public/src/index.js
+++ public/src/index.js
@@ -1,8 +1,10 @@
-//[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...]
-function fib(num) { 
-  if (num <= 1) 
-    return 1;
-    return fib(num - 1) + fib(num - 2);
+//Goal ---->   [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...]
+function fib(num, end) {  
+  if (num.length >= end) {
+    return num;
+  }
+  num.push(num[num.length - 2] + num[num.length - 1]);
+  return fib(num, end);
 }
-
-console.log(fib(5));
+console.log(fib([1,1],10));
+const endArray = ['...'];
